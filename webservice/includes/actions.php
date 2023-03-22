ec056da94e579c9475d1ebd1e37d79839f860d06
Dylan Schoop <42898108+CurleyDS@users.noreply.github.com>
2023-03-22 15:09:35 +0100

    Added suboptions after main info

--- webservice/includes/actions.php
+++ webservice/includes/actions.php
@@ -30,14 +30,72 @@ function getDataDetails($id)
         1 => [
             "id" => 1,
             "name" => "Ambulance",
+            "options" => [
+                1 => [
+                    "id" => 11,
+                    "name" => "Iemand in gevaar",
+                ],
+                2 => [
+                    "id" => 12,
+                    "name" => "Acuut medische zorg nodig",
+                ],
+                3 => [
+                    "id" => 13,
+                    "name" => "Ernstig Ongeluk",
+                ],
+            ],
         ],
         2 => [
             "id" => 2,
             "name" => "Police",
+            "options" => [
+                1 => [
+                    "id" => 21,
+                    "name" => "Lijk",
+                ],
+                2 => [
+                    "id" => 22,
+                    "name" => "Overval",
+                ],
+                3 => [
+                    "id" => 23,
+                    "name" => "Gijzeling",
+                ],
+                4 => [
+                    "id" => 24,
+                    "name" => "Mishandeling",
+                ],
+                5 => [
+                    "id" => 25,
+                    "name" => "Steekpartij",
+                ],
+                6 => [
+                    "id" => 26,
+                    "name" => "Overig",
+                ],
+            ],
         ],
         3 => [
             "id" => 3,
             "name" => "Firefighters",
+            "options" => [
+                1 => [
+                    "id" => 31,
+                    "name" => "Brand",
+                ],
+                2 => [
+                    "id" => 32,
+                    "name" => "Ieamand zit vast",
+                ],
+                3 => [
+                    "id" => 33,
+                    "name" => "Gaslek",
+                ],
+                4 => [
+                    "id" => 34,
+                    "name" => "Bosbrand",
+                ],
+            ],
         ]
     ];
 
